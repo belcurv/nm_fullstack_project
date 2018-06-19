b787f491ef003c93fb7178ee1dbadbbd174afe79
Jay Schwane <jrschwane@uwalumni.com>
2018-06-19 12:26:35 -0500

    Updates Header component and CSS

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,19 +1,23 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import './Header.css';
 
-const Header = ({searchTerm, onSearchSubmit, onInputChange}) => {
-  const search = (
-    <form onSubmit={onSearchSubmit}>
-      <input type="text" placeholder="Search" value={searchTerm} onChange={onInputChange} />
-    </form>
-  );
+const Header = ({ onInputChange, filterTerm }) => (
+  <header className="header">
+    <h1>Guardians, More or Less</h1>
+    <input
+      name="filter"
+      type="text"
+      placeholder="Filter by title or year"
+      value={ filterTerm }
+      onChange={ onInputChange }
+    />
+  </header>
+);
 
-  return (
-    <header className="header">
-      <h1>Open Movie DB</h1>
-      { search }
-    </header>
-  );
+Header.propTypes = {
+  filterTerm    : PropTypes.string,
+  onInputChange : PropTypes.func
 };
 
 export default Header;
