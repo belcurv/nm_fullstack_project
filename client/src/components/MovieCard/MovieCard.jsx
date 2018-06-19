1c9c4eb7d03531db60f0ee1d10b266689eceaf3e
Jay Schwane <jrschwane@uwalumni.com>
2018-06-19 12:26:57 -0500

    Adds MovieCard component and CSS

---
+++ client/src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import './MovieCard.css';
+
+const MovieCard = (movie) => (
+  <div className="movie-card">
+    <div className="movie-card__img">
+      <img src={ movie.Poster } alt={ 'Poster 404' } />
+    </div>
+    <div className="movie-card__text">
+      <h2>{ movie.Title } ({ movie.Year })</h2>
+    </div>
+  </div>
+);
+
+MovieCard.propTypes = {
+  movie : PropTypes.object
+};
+
+export default MovieCard;
