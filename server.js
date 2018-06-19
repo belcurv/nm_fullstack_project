ec1120e537492026abda124dc2a752c8f51243b3
Jay Schwane <jrschwane@uwalumni.com>
2018-06-19 12:25:28 -0500

    Adds cors to API and updates error handler's response

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const express    = require('express');
 const morgan     = require('morgan');
 const bodyParser = require('body-parser');
 const app        = express();
+const cors       = require('cors');
 const port       = process.env.PORT || 3000;
 
 
 /* ================================ CONFIG ================================= */
 
 app.use(morgan('dev'));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 
@@ -23,7 +25,7 @@ app.use('/',    require('./routes/static_routes'));
 // generic error handler
 app.use(function (err, req, res, next) {
   console.log('Error: ' + err.message);
-  res.status(500).send('Something broke');
+  res.status(500).json({ Response : false, message : 'Something broke'});
   next();
 });
 
