f4f352c877729a86cbf717b99b47ba070c324ca7
Jay Schwane <jrschwane@uwalumni.com>
2018-06-20 16:04:32 -0500

    Adds label to filter input box

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,13 +5,15 @@ import './Header.css';
 const Header = ({ onInputChange, filterTerm }) => (
   <header className="header">
     <h1>Guardians, More or Less</h1>
-    <input
-      name="filter"
-      type="text"
-      placeholder="Filter by title or year"
-      value={ filterTerm }
-      onChange={ onInputChange }
-    />
+    <label htmlFor="filter">Filter
+      <input
+        name="filter"
+        type="text"
+        placeholder="by title or year"
+        value={ filterTerm }
+        onChange={ onInputChange }
+      />
+    </label>
   </header>
 );
 
