4cef7c4695285b66df5aa012d864cb9d42169892
Jay Schwane <jrschwane@uwalumni.com>
2018-06-20 23:06:48 -0500

    Adds title param validation util and tests

--- test/utils/validate_title.spec.js
+++ test/utils/validate_title.spec.js
@@ -14,55 +14,62 @@ describe('validateTitle utility', () => {
     assert.isFunction(validateTitle);
   });
 
-  it('should return true for Harry', () => {
+  it('should return true for "Harry"', () => {
     assert.isTrue(validateTitle('Harry'));
   });
 
-  it('should return true for Guardian', () => {
+  it('should return true for "Guardian"', () => {
     assert.isTrue(validateTitle('Guardian'));
   });
 
-  it('should return true for Harry Potter', () => {
-    assert.isTrue(validateTitle('Guardian'));
+  it('should return true for "Harry Potter"', () => {
+    assert.isTrue(validateTitle('Harry Potter'));
   });
 
-  it('should return false for 666', () => {
+  it('should throw exception for 666', () => {
     const call = function() {
       validateTitle(666);
     };
     assert.throws(call, 'Validation Error: Invalid "title": 666');
   });
 
-  it('should return false for booleans', () => {
+  it('should throw exception for booleans', () => {
     const call = function() {
       validateTitle(true);
     };
     assert.throws(call, 'Validation Error: Invalid "title": true');
   });
 
-  it('should return false for arrays', () => {
+  it('should throw exception for arrays', () => {
     const call = function() {
       validateTitle(['Guardian']);
     };
     assert.throws(call, 'Validation Error: Invalid "title": Guardian');
   });
 
-  it('should return false for strings of zero length', () => {
+  it('should throw exception for strings of zero length', () => {
     const call = function() {
       validateTitle('');
     };
     assert.throws(call, 'Validation Error: Invalid "title": ');
   });
 
-  it('should return false for strings of spaces', () => {
+  it('should throw exception for strings of spaces', () => {
     const call = function() {
       validateTitle('      ');
     };
     assert.throws(call, 'Validation Error: Invalid "title": ');
   });
 
-  it('should throw exception when "title" omitted', () => {
-    assert.throws(validateTitle, /Missing required "title" parameter/);
+  it('should throw exception for strings with more than one space', () => {
+    const call = function() {
+      validateTitle('Harry     Potter');
+    };
+    assert.throws(call, 'Validation Error: Invalid "title": ');
+  });
+
+  it('should throw exception if "title" is omitted', () => {
+    assert.throws(validateTitle, 'Missing required "title" parameter');
   });
 
 });
--- utils/validate_title.js
+++ utils/validate_title.js
@@ -6,13 +6,15 @@
  *  A valid title should
  *    1. Be a string
  *    2. Consist of at least 1 alphanumeric character
- *    3. Consist of no more than 100 alphanumeric characters
+ *    3. Optionally contain single spaces between words
+ *    4. Optionally contain surrounding double quotes
+ *    5. Consist of no more than 100 total characters
  *  @param    {String}   title   Candidate movie title
  *  @returns  {Boolean}          True if title is valid
  *  @throws   {Error}            Throws if title is missing or invalid
 */
 module.exports = function validateTitle(title) {
-  const titleRegex = /^"?(\w{1,100}|\s?)"?$/;
+  const titleRegex = /^"?((\w+\s)*\w+){1,100}"?$/;
 
   if (typeof title === 'undefined') {
     throw new Error('Missing required "title" parameter');
