44762ce7d4e68894c39581f000c2e08de59cd7bf
Jay Schwane <jrschwane@uwalumni.com>
2018-06-21 00:17:51 -0500

    Updates Results component to conditionally render either table or grid view

--- client/src/components/Results/Results.jsx
+++ client/src/components/Results/Results.jsx
@@ -2,21 +2,32 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import { filterMovies } from '../../utils';
 import MovieCard from '../MovieCard/MovieCard';
+import MovieTable from '../MovieTable/MovieTable';
+import './Results.css';
 
-const Results = ({ movies, filterTerm }) => {
+const Results = ({ viewStyle, movies, filterTerm }) => {
   const filtered = filterMovies(movies, filterTerm);
 
+  const cards = (
+    filtered.map(movie => <MovieCard key={ movie.imdbID } { ...movie }  /> )
+  );
+
+  const table = (
+    <MovieTable movies={ filtered } />
+  );
+
   return (
-    <section>
-      <h3>Showing { filtered.length } of { movies.length } results:</h3>
-      <div>
-        { filtered.map(movie => <MovieCard key={ movie.imdbID } { ...movie }  /> )}
-      </div>
+    <section className="results">
+      <h3 className="results__heading">
+        Showing { filtered.length } of { movies.length } results
+      </h3>
+      { viewStyle === 'grid' ? cards : table }
     </section>
   );
 };
 
 Results.propTypes = {
+  viewStyle  : PropTypes.string,
   movies     : PropTypes.array,
   filterTerm : PropTypes.string
 };
