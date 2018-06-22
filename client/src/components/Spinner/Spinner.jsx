8a15a12c7d18e70a21788156ebd726de56e85033
Jay Schwane <jrschwane@uwalumni.com>
2018-06-21 23:11:32 -0500

    Client: tweaks Spinner position and size

--- client/src/components/Spinner/Spinner.jsx
+++ client/src/components/Spinner/Spinner.jsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
+import React from 'react';
 import './Spinner.css';
 
 const Spinner = () => (
   <div className="spinner">
-    <svg className="spinner__svg" viewBox="0 0 100 100" width="40" height="40">
+    <svg className="spinner__svg" viewBox="0 0 100 100" width="50" height="50">
       <circle cx="50" cy="50" r="42" transform="rotate(-90,50,50)" />
     </svg>
   </div>
